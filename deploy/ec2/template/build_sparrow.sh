20b8673332633adc01cde27c44dc722d41885b0d
Patrick Wendell <pwendell@gmail.com>
2012-02-06 15:42:55 -0800

    Adding deployment scripts for ec2

---
+++ deploy/ec2/template/build_sparrow.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Build Sparrow locally
+
+INSTALL_DIR=~/sparrow/
+
+if [ ! -d "$INSTALL_DIR" ]; then
+  mkdir $INSTALL_DIR
+fi;
+
+cd /tmp/
+
+# Remove old install if here
+if [ -d "sparrow" ]; then
+  rm -rf sparrow
+fi;
+
+git clone git://github.com/radlab/sparrow.git
+cd sparrow
+mvn package -Dmaven.test.skip=true
+cp /tmp/sparrow/target/sparrow-1.0-SNAPSHOT.jar $INSTALL_DIR
---
+++ deploy/ec2/template/deploy_sparrow.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Deploy built version of Sparrow on frontends and backends.
+
+FRONTENDS=`cat frontends.txt`
+BACKENDS=`cat backends.txt`
+SSH_OPTS="-o StrictHostKeyChecking=no -o ConnectTimeout=5"
+
+for fe in $FRONTENDS; do
+  rsync -e "ssh $SSH_OPTS" -az ~/ $fe:~/ &
+  sleep 0.3
+done
+
+for fe in $BACKENDS; do
+  rsync -e "ssh $SSH_OPTS" -az ~/ $fe:~/ &
+  sleep 0.3
+done
+wait
