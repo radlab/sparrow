23378c7ed2924534f537644d142cb3fad8746d04
Ubuntu <ubuntu@ip-10-195-97-131.ec2.internal>
2013-03-20 00:27:28 +0000

    Some changes to TPCH runner script

--- deploy/ec2/tpch_experiments.sh
+++ deploy/ec2/tpch_experiments.sh
@@ -2,23 +2,23 @@ import os
 import subprocess
 import time
 
-num_nodes = 50
+num_nodes = 18
 wait_delay = 24 * 60
 results_dirname = "results"
-partitions = 30
-ratios = [(1, 1), (2, 2)] #-p, -q
-rates = [500, 450, 400, 350]
+partitions = 6
+ratios = [(2, 2)] #-p, -q
+rates = [400]
 
 def run_cmd(cmd):
   subprocess.check_call(cmd, shell=True)
 
-restart_cmd = "./ec2-exp.sh -i ~/.ssh/id_rsa restart-spark-shark -m sparrow"
-start_cmd = "./ec2-exp.sh -i ~/.ssh/id_rsa start-shark-tpch"
+restart_cmd = "./ec2-exp.sh -i ~/.ssh/patkey.pem restart-spark-shark -m sparrow"
+start_cmd = "./ec2-exp.sh -i ~/.ssh/patkey.pem start-shark-tpch"
 
 for rate in rates:
   for (p, q) in ratios:
     dep_cmd = "./ec2-exp.sh deploy -g nsdi-patrick -s dev-sparrow-newcode " +\
-      "-i ~/.ssh/id_rsa -p %s -q %s -u %s -v %s" % (p, q, partitions, rate)
+      "-i ~/.ssh/patkey.pem -p %s -q %s -u %s -v %s" % (p, q, partitions, rate)
     run_cmd(dep_cmd)
     run_cmd(restart_cmd)
     run_cmd(start_cmd)
@@ -27,6 +27,6 @@ for rate in rates:
     collect_dir = "%s/%s_%s_%s_%s" % (results_dirname, num_nodes, rate, p, q)
     if not os.path.exists(collect_dir):
       os.mkdir(collect_dir)
-    collect_cmd = "./ec2-exp.sh -i ~/.ssh/id_rsa collect-logs --log-dir=%s" % \
+    collect_cmd = "./ec2-exp.sh -i ~/.ssh/patkey.pem collect-logs --log-dir=%s" % \
       collect_dir
     run_cmd(collect_cmd)
