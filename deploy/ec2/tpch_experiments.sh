55c61ec76d09b9491ceb1cebedc90222cb0f1db8
Ubuntu <ubuntu@ip-10-195-177-233.ec2.internal>
2013-03-25 16:59:14 +0000

    Some changes to parsing and run scripts

--- deploy/ec2/tpch_experiments.sh
+++ deploy/ec2/tpch_experiments.sh
@@ -2,23 +2,23 @@ import os
 import subprocess
 import time
 
-num_nodes = 18
-wait_delay = 24 * 60
+num_nodes = 40
+wait_delay = 15 * 60
 results_dirname = "results"
-partitions = 6
-ratios = [(2, 2), (1, 1)] #-p, -q
-rates = [76]
-backend_mem = "10g"
+partitions = 13
+ratios = [(1, 1), (2, 2)] #-p, -q
+rates = [125]
+backend_mem = "5g"
 
 def run_cmd(cmd):
   subprocess.check_call(cmd, shell=True)
 
-restart_cmd = "./ec2-exp.sh -i ~/.ssh/patkey.pem restart-spark-shark -m sparrow"
-start_cmd = "./ec2-exp.sh -i ~/.ssh/patkey.pem start-shark-tpch"
+restart_cmd = "./ec2-exp.sh -i ~/.ssh/patkey.pem restart-spark-shark sparrow -m sparrow"
+start_cmd = "./ec2-exp.sh -i ~/.ssh/patkey.pem start-shark-tpch sparrow"
 
 for rate in rates:
   for (p, q) in ratios:
-    dep_cmd = "./ec2-exp.sh deploy -g nsdi-patrick -s dev-sparrow-newcode " +\
+    dep_cmd = "./ec2-exp.sh deploy sparrow -g nsdi-patrick -s dev-sparrow-newcode " +\
       "-i ~/.ssh/patkey.pem -p %s -q %s -u %s -v %s --spark-backend-mem=%s" % (
       p, q, partitions, rate, backend_mem)
     run_cmd(dep_cmd)
@@ -29,6 +29,6 @@ for rate in rates:
     collect_dir = "%s/%s_%s_%s_%s" % (results_dirname, num_nodes, rate, p, q)
     if not os.path.exists(collect_dir):
       os.mkdir(collect_dir)
-    collect_cmd = "./ec2-exp.sh -i ~/.ssh/patkey.pem collect-logs --log-dir=%s" % \
+    collect_cmd = "./ec2-exp.sh -i ~/.ssh/patkey.pem collect-logs sparrow --log-dir=%s" % \
       collect_dir
     run_cmd(collect_cmd)
