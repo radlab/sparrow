d85100860c3784aba931b138fe7b5608e67e1eb8
Kay Ousterhout <kayousterhout@gmail.com>
2013-03-26 13:07:04 -0700

    Fixed compile error

--- src/main/java/edu/berkeley/sparrow/statestore/ConfigStateStoreState.java
+++ src/main/java/edu/berkeley/sparrow/statestore/ConfigStateStoreState.java
@@ -12,19 +12,19 @@ import edu.berkeley.sparrow.daemon.util.ConfigUtil;
  * A {@link StateStoreState} which reads schedulers and node monitors from a static
  * configuration file. We expect this file to contain a complete list of node monitors
  * and schedulers which are waiting for the StateStore to contact them. If a different
- * node monitor or scheduler contacts the state store, this class throws a 
- * RuntimeException. 
+ * node monitor or scheduler contacts the state store, this class throws a
+ * RuntimeException.
  */
 public class ConfigStateStoreState implements StateStoreState {
   private List<InetSocketAddress> nodeMonitors = new ArrayList<InetSocketAddress>();
   private List<InetSocketAddress> schedulers = new ArrayList<InetSocketAddress>();
-  
+
   @Override
   public void initialize(Configuration conf) {
-    nodeMonitors.addAll(ConfigUtil.parseBackends(conf).keySet());
+    nodeMonitors.addAll(ConfigUtil.parseBackends(conf));
     schedulers.addAll(ConfigUtil.parseSchedulers(conf));
   }
-  
+
   @Override
   public List<InetSocketAddress> getInitialSchedulers() {
     return schedulers;
@@ -50,12 +50,12 @@ public class ConfigStateStoreState implements StateStoreState {
   @Override
   public void signalActiveScheduer(InetSocketAddress scheduler) {
     throw new RuntimeException("Not expecting new scheduler in " +
-        "configbased deployment mode."); 
+        "configbased deployment mode.");
   }
 
   @Override
   public void signalActiveNodeMonitor(InetSocketAddress nodeMonitor) {
     throw new RuntimeException("Not expecting new scheduler in " +
-        "configbased deployment mode."); 
+        "configbased deployment mode.");
   }
 }
