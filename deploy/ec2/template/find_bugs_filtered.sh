d54f573f10d48ae2ce440a6eb0adc9fe426eadd4
Ubuntu <ubuntu@ip-10-188-215-196.ec2.internal>
2013-03-27 17:30:23 +0000

    Modified version of find bugs

---
+++ deploy/ec2/template/find_bugs_filtered.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Filters out some known issues
+host=`ec2metadata  |grep public-hostname | cut -d " " -f 2`
+echo $host
+cat /disk1/sparrow/* |egrep "ERROR|Exception" | grep -v Buff | wc  
