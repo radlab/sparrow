32d13fecf1d9616b6ad2cc54949dc3d6d437f2dd
Ubuntu <ubuntu@ip-10-68-189-106.ec2.internal>
2013-03-27 06:23:29 +0000

    Allowing diferent cluster names

--- deploy/ec2/tpch_experiments.sh
+++ deploy/ec2/tpch_experiments.sh
@@ -2,25 +2,25 @@ import os
 import subprocess
 import time
 
-num_nodes = 40
-wait_delay = 15 * 60
+num_nodes = 20
+wait_delay = 20 * 60
 results_dirname = "results"
-partitions = 13
-ratios = [(1, 1), (2, 2)] #-p, -q
-rates = [125]
+partitions = 6
+ratios = [(0, 0), (2, 2)] #-p, -q
+rates = [500]
 backend_mem = "5g"
+cluster_name = "small"
 
 def run_cmd(cmd):
   subprocess.check_call(cmd, shell=True)
 
-restart_cmd = "./ec2-exp.sh -i ~/.ssh/patkey.pem restart-spark-shark sparrow -m sparrow"
-start_cmd = "./ec2-exp.sh -i ~/.ssh/patkey.pem start-shark-tpch sparrow"
+restart_cmd = "./ec2-exp.sh -i ~/.ssh/patkey.pem restart-spark-shark %s -m sparrow" % cluster_name
+start_cmd = "./ec2-exp.sh -i ~/.ssh/patkey.pem start-shark-tpch %s" % cluster_name
 
 for rate in rates:
   for (p, q) in ratios:
-    dep_cmd = "./ec2-exp.sh deploy sparrow -g nsdi-patrick -s dev-sparrow-newcode " +\
-      "-i ~/.ssh/patkey.pem -p %s -q %s -u %s -v %s --spark-backend-mem=%s" % (
-      p, q, partitions, rate, backend_mem)
+    dep_cmd = "./ec2-exp.sh deploy %s -g patrick-sosp -s dev-sparrow-newcode -i ~/.ssh/patkey.pem -p %s -q %s -u %s -v %s --spark-backend-mem=%s" % (
+      cluster_name, p, q, partitions, rate, backend_mem)
     run_cmd(dep_cmd)
     run_cmd(restart_cmd)
     run_cmd(start_cmd)
@@ -29,6 +29,18 @@ for rate in rates:
     collect_dir = "%s/%s_%s_%s_%s" % (results_dirname, num_nodes, rate, p, q)
     if not os.path.exists(collect_dir):
       os.mkdir(collect_dir)
-    collect_cmd = "./ec2-exp.sh -i ~/.ssh/patkey.pem collect-logs sparrow --log-dir=%s" % \
-      collect_dir
+    collect_cmd = "./ec2-exp.sh -i ~/.ssh/patkey.pem collect-logs %s --log-dir=%s" % (cluster_name, collect_dir)
     run_cmd(collect_cmd)
+
+    analysis_dir = "%s/analysis" % collect_dir
+    if not os.path.exists(analysis_dir):
+      os.mkdir(analysis_dir)
+
+    run_cmd("gunzip %s/*.log.gz" % collect_dir)
+    run_cmd("python /home/ubuntu/sparrow/src/main/python/parse_tpch_logs.py 600 800 " \
+           "/home/ubuntu/sparrow/deploy/ec2/%s/shark_* > /home/ubuntu/sparrow/deploy/ec2/%s/tpch_results" % (collect_dir, analysis_dir))
+
+    run_cmd("/home/ubuntu/sparrow_old/src/main/python/parse_logs.sh start_sec=600 " \
+           "end_sec=800 log_dir=/home/ubuntu/sparrow/deploy/ec2/%s/ output_dir=/home/ubuntu/sparrow/deploy/ec2/%s/ > /home/ubuntu/sparrow/deploy/ec2/%s/parse_results" % (
+               collect_dir, analysis_dir, analysis_dir))
+    run_cmd("gzip %s/*.log" % collect_dir)
